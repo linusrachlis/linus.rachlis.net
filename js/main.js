fb066a64822571e19eb3fb6e477219405dcc9edc
Linus Rachlis <linus@linusrachlis.ca>
2014-11-03 00:37:43 -0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,34 @@
+;$(function () {
+  var $nav = $("nav")
+    , $navUl = $("nav ul")
+    , $bg = $("img.bg")
+    , $shift = $("img.shift")
+    , $navAnchors = $("nav ul li a")
+    , $window = $(window)
+
+  $window.resize(function() {
+    console.log($bg.width() + ", " + $bg.height())
+    $nav.width($bg.width())
+    $nav.height($bg.height())
+  })
+  .resize()
+
+  // Make "idle" mean that no key is being hovered
+  $navAnchors
+    .mouseover(function () {
+      $navUl.removeClass("idle")
+    })
+    .mouseout(function () {
+      $navUl.addClass("idle")
+    })
+
+  // @ and * also make shift light up
+  $("nav ul li.contact a, nav ul li.experiments a")
+    .mouseover(function () {
+      console.log("mouseover")
+      $shift.addClass("on")
+    })
+    .mouseout(function () {
+      $shift.removeClass("on")
+    })
+})
