9bd2c665249194abb608315c637abe6f652d6b9d
Linus Rachlis <linus@linusrachlis.ca>
2014-11-08 01:28:51 -0500

    Add contact page and properly scope the CSS for different pages

---
+++ js/contact.js
@@ -0,0 +1,21 @@
+;$(function () {
+  var $contactContainer = $(".container")
+    , $contactMethodsUl = $(".container ul")
+    , $bg = $("img.bg")
+    , $window = $(window)
+
+  $window
+    .resize(function() {
+      // Continuously copy bg <img> dimensions to ul element so that its children
+      // can be scaled. Also scale font size explicitly, since using a % for font-size
+      // doesn't accomplish what I want.
+      var bgHeight = $bg.height()
+      $contactContainer
+        .width($bg.width())
+        .height(bgHeight)
+      $contactMethodsUl
+        .css("font-size", (bgHeight * 0.04) + "px")
+    })
+    // Init scaling by running the above code on DOM ready.
+    .resize()
+})
