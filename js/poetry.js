347563f8c1eca588927ec9ff3e6dfda0e760c55e
Linus Rachlis <linus@linusrachlis.ca>
2014-11-09 18:54:38 -0500

    All the poetry!

--- js/poetry.js
+++ js/poetry.js
@@ -1,6 +1,9 @@
 ;$(function () {
   var $poetryContainer = $(".container")
     , $poemsDiv = $(".poems")
+    , $poemDiv = $(".poem")
+    , $poemText = $(".poem .text")
+    , $backButton = $(".back")
     , $bg = $("img.bg")
     , $window = $(window)
 
@@ -13,9 +16,27 @@
       $poetryContainer
         .width($bg.width())
         .height(bgHeight)
-      $poemsDiv
-        .css("font-size", (bgHeight * (16/955)) + "px")
+      $poemsDiv.css("font-size", (bgHeight * (16/955)) + "px")
+      $poemText.css("font-size", (bgHeight * (17/955)) + "px")
     })
     // Init scaling by running the above code when the big image is loaded.
     .load(function () { $window.resize() })
+  
+  // Open poem
+  $(".poems a").click(function (e) {
+    e.preventDefault()
+    var $this = $(this)
+    $poemText.load($this.attr('href') + " .poem-text")
+    $poemsDiv.add($backButton).fadeOut('fast', function () {
+      $poemDiv.fadeIn('slow')
+    })
+  })
+  
+  // Close poem
+  $poemDiv.click(function () {
+    $poemDiv.fadeOut('fast', function () {
+      $poemText.text("...")
+      $poemsDiv.add($backButton).fadeIn('fast')
+    })
+  })
 })
