70eaaffc00d01cac2b6fe69f689b4869c3127626
Linus Rachlis <linus@linusrachlis.ca>
2014-11-09 12:59:18 -0500

    Beginning of work on poetry page

---
+++ js/poetry.js
@@ -0,0 +1,21 @@
+;$(function () {
+  var $poetryContainer = $(".container")
+    , $poemsUl = $(".container > ul")
+    , $bg = $("img.bg")
+    , $window = $(window)
+
+  $window
+    .resize(function() {
+      // Continuously copy bg <img> dimensions to ul element so that its children
+      // can be scaled. Also scale font size explicitly, since using a % for font-size
+      // doesn't accomplish what I want.
+      var bgHeight = $bg.height()
+      $poetryContainer
+        .width($bg.width())
+        .height(bgHeight)
+      $poemsUl
+        .css("font-size", (bgHeight * (15/955)) + "px")
+    })
+    // Init scaling by running the above code when the big image is loaded.
+    .load(function () { $window.resize() })
+})
