df0c834fc367b7d846028a8628309f20047dc54e
Linus Rachlis <linus@linusrachlis.ca>
2014-11-10 16:01:28 -0500

    Give homepage a touch-friendly mode that's not so overly mysterious.
    Apparently Modernizr.touch isn't the best, but for now it's better
    than nothing.

--- js/index.js
+++ js/index.js
@@ -3,36 +3,62 @@
     , $navUl = $("nav ul")
     , $bg = $("img.bg")
     , $shift = $("img.shift")
-    , $navAnchors = $("nav ul li a")
+    , $navLis = $("nav ul li")
     , $window = $(window)
 
-  $navAnchors
-    .on('mouseover focus', function () {
+  $navLis
+    .on('mouseover', function () {
+      var $this = $(this)
       // Make "idle" mean that no key is being hovered
       $navUl.removeClass("idle")
-      // Label appears
-      $(this).data("label").addClass("active")
+      if (!Modernizr.touch) {
+        // Label appears
+        $this.data("label").addClass("active")
+      }
     })
-    .on('mouseout blur', function () {
+    .on('mouseout', function () {
+      var $this = $(this)
       // Go back to cycling keys
       $navUl.addClass("idle")
-      // Label disappears
-      $(this).data("label").removeClass("active")
+      if (!Modernizr.touch) {
+        // Label disappears
+        $this.data("label").removeClass("active")
+      }
     })
     .each(function () {
       var $this = $(this)
-        , $label = $("<div class='label'></div>").html($this.html())
-      $this.data("label", $label)
-      $nav.prepend($label)
+        , $keyAnchor = $this.children(".key")
+        , labelBaseHtml = (Modernizr.touch)
+            ? ("<a href='" + $keyAnchor.attr('href') + "' class='label'></a>")
+            : "<span class='label'></span>"
+        , $label = $(labelBaseHtml).html($keyAnchor.html())
+      if (Modernizr.touch) {
+        // Put label anchor in this LI, alongside keyAnchor, so it can
+        // be positioned relative to this LI.
+        $this.prepend($label)
+      } else {
+        $nav.prepend($label)
+        $this.data("label", $label)
+      }
     })
 
   // @ and * also make shift light up
-  $("nav ul li.contact a, nav ul li.experiments a")
-    .on('mouseover focus', function () {
+  $("nav ul li.contact, nav ul li.experiments")
+    .on('mouseover', function () {
       $shift.addClass("on")
     })
-    .on('mouseout blur', function () {
+    .on('mouseout', function () {
       $shift.removeClass("on")
+    })
+
+  // Keyboard focus on .key anchors should trigger same as hover
+  // on LIs.
+  $("nav ul li .key")
+    .on('focus', function () {
+      $(this).parent().addClass('focus').mouseover()
+    })
+    .on('blur', function () {
+      $(this).parent().removeClass('focus').mouseout()
     })
 
   $window
@@ -43,7 +69,7 @@
       var bgHeight = $bg.height()
       $nav.width($bg.width())
       $nav.height(bgHeight)
-      $("nav .label").css("font-size", (bgHeight * 0.04) + "px")
+      $("nav").css("font-size", (bgHeight * 0.04) + "px")
     })
     // Init scaling by running the above code when the big image is loaded.
     .load(function () { $window.resize() })
